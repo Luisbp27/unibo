90deb7c7881b11ac2c7a489f0a00563d5e000420
Luisbp27 <luisbarcapons@gmail.com>
2023-10-25 11:46:00 +0200

    lab3 donw

--- Distributed Software Systems/lab3/server.py
+++ Distributed Software Systems/lab3/server.py
@@ -1,6 +1,7 @@
 import grpc
 import greet_pb2
 import greet_pb2_grpc
+import concurrent.futures
 
 class GreetServicer(greet_pb2_grpc.GreetServiceServicer):
     def Greet(self, request, context):
@@ -8,7 +9,7 @@ class GreetServicer(greet_pb2_grpc.GreetServiceServicer):
         return response
 
 def run_server():
-    server = grpc.server(grpc.InsecureServer())
+    server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))
     greet_pb2_grpc.add_GreetServiceServicer_to_server(GreetServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
