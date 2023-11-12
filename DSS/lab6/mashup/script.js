645ec0f540a8ab1ff7f4dbc42a923bc03b985def
Luisbp27 <luisbarcapons@gmail.com>
2023-11-12 21:24:30 +0100

    lab6 mashup done

--- DSS/lab6/mashup/script.js
+++ DSS/lab6/mashup/script.js
@@ -1,26 +1,49 @@
-async function getWeatherData() {
+const apiKey1 = '0a03a25cf192bc3eaa74a7784b0b69c9'; // Replace with your API key
+const apiKey2 = 'cfe493cdd37e4489bc0185641231211'; // Replace with your API key
+
+async function getWeather() {
     const location = document.getElementById('location').value;
+    const url1 = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey1}`;
+    const url2 = `http://api.weatherapi.com/v1/current.json?key=${apiKey2}&q=${location}&aqi=no`;
 
-    // Fetch data from Weather.com API
-    const weatherComResponse = await fetch(`https://api.weather.com/data/2.5/weather?q=${location}&units=metric&appid=YOUR_WEATHERCOM_API_KEY`);
-    const weatherComData = await weatherComResponse.json();
+    try {
+        const [data1, data2] = await Promise.all([
+            fetch(url1).then(response => response.json()),
+            fetch(url2).then(response => response.json())
+        ]);
 
-    // Fetch data from OpenWeatherMap API
-    const openWeatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=YOUR_OPENWEATHERMAP_API_KEY`);
-    const openWeatherData = await openWeatherResponse.json();
-
-    // Process and display data
-    displayWeatherData(weatherComData, openWeatherData);
+        // Process and display the weather data
+        displayWeather(data1, data2);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
 }
 
-function displayWeatherData(weatherComData, openWeatherData) {
-    // Process and display data in the .weather-results div
-    // Example: Display current temperature from Weather.com and OpenWeatherMap
-    const weatherResultsDiv = document.querySelector('.weather-results');
-    weatherResultsDiv.innerHTML = `
-        <h2>Weather Forecast for ${weatherComData.name}</h2>
-        <p>Temperature (Weather.com): ${weatherComData.main.temp}°C</p>
-        <p>Temperature (OpenWeatherMap): ${openWeatherData.main.temp}°C</p>
-        <!-- Add more weather information here -->
+// script.js
+function displayWeather(data1, data2) {
+    const weatherInfo = document.getElementById('weather-info');
+
+    const location = `${data1.name}, ${data1.sys.country}`;
+    const tempOWM = `${(data1.main.temp - 273.15).toFixed(2)}°C`; // Convert temperature from Kelvin to Celsius
+    const tempWeatherAPI = `${data2.current.temp_c}°C`;
+
+    const descriptionOWM = data1.weather[0].description;
+    const descriptionWeatherAPI = data2.current.condition.text;
+
+    const html = `
+        <h2>${location}</h2>
+        <div class="weather-details">
+            <div>
+                <p><strong>Current Temperature (OpenWeatherMap):</strong> ${tempOWM}</p>
+                <p><strong>Description (OpenWeatherMap):</strong> ${descriptionOWM}</p>
+            </div>
+            <div>
+                <p><strong>Current Temperature (WeatherAPI):</strong> ${tempWeatherAPI}</p>
+                <p><strong>Description (WeatherAPI):</strong> ${descriptionWeatherAPI}</p>
+            </div>
+        </div>
     `;
+
+    weatherInfo.innerHTML = html;
 }
+
