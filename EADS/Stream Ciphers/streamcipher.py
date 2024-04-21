0ff6899c7be5e84f6879f454b754f9fd2e640214
Luisbp27 <luisbarcapons@gmail.com>
2024-04-21 13:14:59 +0200

    Añadido código de prueba para iterar y obtener elementos de sg en test_EncryptionEagles.ipynb

--- EADS/Stream Ciphers/streamcipher.py
+++ EADS/Stream Ciphers/streamcipher.py
@@ -2,29 +2,58 @@ import functools
 from operator import xor
 
 class LFSR:
+    """ Linear Feedback Shift Register (LFSR) class
+    LFSR is a shift register whose input bit is a linear function of its previous state.
+    The output bit is the last bit of the state.
+
+    Attributes:
+    - poly: Polynomial of the LFSR
+    - length: Length of the LFSR
+    - state: Initial state of the LFSR
+    - output: Output bit of the LFSR
+    - feedback: Feedback bit of the LFSR
+
+    Methods:
+    - __init__: Constructor
+    - __len__: Returns the length of the LFSR
+    - __iter__: Iterator
+    - __next__: Returns the next output bit of the LFSR
+    - run_steps: Execute N LFSR steps and returns the corresponding output list of bool
+    - cycle: Returns a list of boolean values representing the full LFSR cycle
+    - __str__: Returns a string representation of the LFSR
+    """
 
     def __init__(self, poly, state=None):
+        """ Constructor
+
+        Args:
+            poly: Polynomial of the LFSR
+            state: Initial state of the LFSR. Default is None.
+        """
+
         self.poly = poly
-        self.length = max(poly)
+        self.length = len(poly)
         self.state = state
-        self.output = self.state[:1]
-        self.feedback = functools.reduce(xor, [a & b for a, b in zip(self.poly[1:], self.state)])
 
         # If no state is provided, initialize all bits to 1
         if state is None:
             self.state = [1] * self.length
         else:
-            # If the state is provided, it must have the same length as the polynomial
-            if len(state) < self.length:
-                # If the state is shorter than the polynomial, fill the remaining bits with 0
-                self.state = [0] * (self.length - len(state)) + state
-            else:
-                self.state = state
+            # Convert the state to a list of bits
+            self.state = [int(i) for i in list(bin(state)[2:])]
+            self.state = [0]*(self.length - len(self.state)) + self.state if (self.length > len(self.state)) else self.state
+
+        print(f"Initial state: {self.state}")
+
+        self.output = self.state[:1]
+        self.feedback = functools.reduce(xor, [a & b for a, b in zip(self.poly[1:], self.state)])
 
     def __len__(self):
+        """ Returns the length of the LFSR """
         return self.length
 
     def __iter__(self):
+        """ Iterator """
         return self
 
     def __next__(self):
@@ -44,7 +73,8 @@ class LFSR:
 
     def run_steps(self, n=1):
         """ Execute N LFSR steps and returns the corresponding output list of bool """
-        return [next(self) for _ in range(n)]
+        list_of_bool = [next(self) for _ in range(n)]
+        return list_of_bool
 
     def cycle(self, state=None):
         """ Returns a list of boolean values representing the full LFSR cycle """
@@ -52,7 +82,8 @@ class LFSR:
             self.state = state
 
         # The number of steps to perform is 2^L - 1, where L is the length of the LFSR
-        return [next(self) for _ in range(2**self.length - 1)]
+        list_of_bool = [next(self) for _ in range(2**self.length - 1)]
+        return list_of_bool
 
     def __str__(self):
         """ Returns a string representation of the LFSR """
@@ -60,55 +91,142 @@ class LFSR:
 
 
 def berlekamp_massey(b):
-    m = 0
-    r = 1
-    N = len(b)
+    """ Berlekamp-Massey algorithm
+    Berlekamp-Massey algorithm is a method to find the shortest LFSR that can generate a given binary sequence.
+
+    Args:
+        b : List of binary values
+
+    Returns:
+        poly: Polynomial of the LFSR
+    """
+
+    m = 0 # LFSR length
+    r = 1 # LFSR feedback polynomial degree
+    N = len(b) # Length of the input sequence
     P = [1] # P(x) = 1
     Q = [1] # Q(x) = 1
-    R = []
+    R = [] # R(x) = 0
 
     for t in range(N):
         d = 0
         for j in range(m+1):
-            d = xor(d, P[j] and b[t-j]) # Discrepancy computation
+            d = xor(d, P[j] and b[t-j]) # Calculate the discrepancy
         if (d == 1):
-            Qtemp = Q.copy()        #
-            for i in range(r):      #
-                Qtemp = [0] + Qtemp # Qtemp(x) = Q(x)*(x^r)
+            # If the discrepancy is 1, update the new polynomial R(x)
+            Qtemp = Q.copy()
+            for i in range(r):
+                Qtemp = [0] + Qtemp
             if (2*m <= t):
-                R = P.copy() # R(x) = P(x)
-                for i in range(min(len(Qtemp), len(P))): #
-                    P[i] = xor(P[i], Qtemp[i])           #
-                if (len(Qtemp) > len(P)):                #
-                    P += Qtemp[len(P):]                  # P(x) = P(x) + Qtemp(x)
-                Q = R.copy() # Q(x) = R(x)
+                R = P.copy() # R(x) <- P(x)
+                for i in range(min(len(Qtemp), len(P))):
+                    P[i] = xor(P[i], Qtemp[i])
+                if (len(Qtemp) > len(P)):
+                    P += [0] * (len(Qtemp) - len(P) + r)
+                    for i in range(len(Qtemp)):
+                        P[i + r] = xor(P[i + r], Qtemp[i])  # P(x) = P(x) + Qtemp(x)*x^r
+                Q = R.copy() # Q(x) <- R(x)
                 m = t + 1 - m
                 r = 0
             else:
-                for i in range(min(len(Qtemp), len(P))): #
-                    P[i] = xor(P[i], Qtemp[i])           #
-                if (len(Qtemp) > len(P)):                #
-                    P += Qtemp[len(P):]                  # P(x) = P(x) + Qtemp(x)
-        r += 1
+                for i in range(min(len(Qtemp), len(P))):
+                    P[i] = xor(P[i], Qtemp[i])
+                if len(Qtemp) > len(P):
+                    P += [0] * (len(Qtemp) - len(P) + r)
+                    for i in range(len(Qtemp)):
+                        P[i + r] = xor(P[i + r], Qtemp[i])  # P(x) = P(x) + Qtemp(x)*x^r
+        r += 1 # r <- r + 1
 
-    # I need a list made of integers which tell me where in the polynomial I have a coefficient 1 or 0:
-    # for a polynomial like 1 + 1x + 0x^2 + 1x^3, given by the algorithm as [1,1,0,1],
-    # I get a list like [3,1,0], corresponding to x^3 + x + 1
-    poly = []
-    for i, x in enumerate(P):
-        if (x):
-            poly = [i] + poly
+    poly = [i for i, x in enumerate(P) if x] # P(x)
 
     return poly
 
 
 class ShrinkingGenerator:
+    """ Shrinking Generator based on two LFSRs
+    Shrinking Generator is a stream cipher that generates a key stream by combining two LFSRs.
+    The output bit is the result of the bitwise AND operation between the output bits of the two LFSRs.
 
-    def __init__(self):
-        pass
+    Attributes:
+    - polyA: Polynomial of the first LFSR
+    - polyS: Polynomial of the second LFSR
+    - stateA: Initial state of the first LFSR
+    - stateS: Initial state of the second LFSR
+    - lfsrA: First LFSR
+    - lfsrS: Second LFSR
+    - output: Output bit of the Shrinking Generator
+
+    Methods:
+    - __init__: Constructor
+    - __iter__: Iterator
+    - __next__: Returns the next output bit of the Shrinking Generator
+    """
+
+    def __init__(self, polyA=None, polyS=None, stateA=None, stateS=None):
+        """ Constructor
+
+        Args:
+            polyA: Polynomial of the first LFSR. Default is None.
+            polyS: Polynomial of the second LFSR. Default is None.
+            stateA: Initial state of the first LFSR. Default is None.
+            stateS: Initial state of the second LFSR. Default is None.
+        """
+
+        self.lfsrA = LFSR(polyA, stateA)
+        self.lfsrS = LFSR(polyS, stateS)
+        self.output = None
+
+    def __iter__(self):
+        """ Iterator
+
+        Returns:
+            self: Shrinking Generator
+        """
+
+        return self
+
+    def __next__(self):
+        """ Returns the next output bit of the Shrinking Generator
+
+        Returns:
+            output: Output bit of the Shrinking Generator
+        """
+
+        a = next(self.lfsrA)
+        s = next(self.lfsrS)
+        self.output = a & s
+
+        return self.output
 
 class SelfShrinkingGenerator:
+    """ Self-Shrinking Generator based on a LFSR
+    Self-Shrinking Generator is a stream cipher that generates a key stream by using a single LFSR.
+    The output bit is the result of the bitwise AND operation between the output bit of the LFSR and a selection bit.
 
-    def __init__(self):
-        pass
+    Attributes:
+    - poly: Polynomial of the LFSR
+    - selection_bit: Selection bit
+    - state: Initial state of the LFSR
+    - lfsr: LFSR
+    - output: Output bit of the Self-Shrinking Generator
+
+    Methods:
+    - __init__: Constructor
+    - __iter__: Iterator
+    - __next__: Returns the next output bit of the Self-Shrinking Generator
+    """
+
+    def __init__(self, poly=None, selection_bit=None, state=None):
+        self.lfsr = LFSR(poly, state)
+        self.sbit = selection_bit
+        self.output = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """ Returns the next output bit of the Self-Shrinking Generator """
+        s = next(self.lfsr)
+        self.output = s & self.sbit
+        return self.output
 
